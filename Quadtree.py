acea484948b36579ecb80bee9adb7df42adfdf17
Lazca <35242788+Lazca@users.noreply.github.com>
2020-08-29 13:40:50 -0500

    Create Quadtree.py

---
+++ Quadtree.py
@@ -0,0 +1,24 @@
+class Point():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+class Node():
+    def __init__(self, x0, y0, w, h , points):
+        self.x0=x0
+        self.y0=y0
+        self.width=w
+        self.height=h
+        self.points=points
+        self.children=[]
+
+    def get_points():
+        return self.points
+
+    def get_height():
+        return self.height
+
+    def get_width():
+        return self.get_width
+
+class Quad
