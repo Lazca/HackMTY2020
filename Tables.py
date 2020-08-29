8ef400f77a680875238834454a9aa5a0e75ee8c1
JairAntonio22 <jair.loranca@gmail.com>
2020-08-29 18:58:17 -0500

    Added person width

--- Tables.py
+++ Tables.py
@@ -1,4 +1,4 @@
-# Module Tables.py
+# Module Tables.py 
 
 import math
 
@@ -8,7 +8,7 @@ class Table:
         self.capacity = 0
 
     def getCapacity(self):
-        return self.capacity
+        return self.position
 
     def setCapacity(self, capacity):
         self.capacity = capacity
@@ -32,11 +32,11 @@ def positionTaken(tables, position, minDist):
 
 def inBound(boundary, x, y):
     x0, y0, width, height = boundary
-    return (x0 <= x <= width) and (y0 <= y <= height)
+    return x0 <= x <= width and y0 <= y <= height
 
-def getTableArrangement(boundary, minDist, tableRadius):
+def getTableArrangement(boundary, minDist, tableRadius, personWidth):
     tables = []
-    length = minDist + tableRadius
+    length = 2*minDist + tableRadius + personWidth
 
     def addTable(x, y):
         if not inBound(boundary, x, y):
@@ -47,8 +47,8 @@ def getTableArrangement(boundary, minDist, tableRadius):
             tables.append(Table((x,y)))
 
             for i in range(6):
-                newX = 2*length*math.cos(math.pi*i/3)
-                newY = 2*length*math.sin(math.pi*i/3)
+                newX = length*math.cos(math.pi*i/3)
+                newY = length*math.sin(math.pi*i/3)
                 addTable(x + newX, y + newY)
 
     addTable(0,0)
@@ -56,7 +56,8 @@ def getTableArrangement(boundary, minDist, tableRadius):
     return tables
 
 if __name__ == "__main__":
-    tables = getTableArrangement((0,0,7.5,5), 1.5, 1)
+    tables = getTableArrangement((0,0,7.5,5), 1.5, 1, 1)
 
     for table in tables:
         print(f"{table.position[0]}\t{table.position[1]}\t{table.capacity}")
+
