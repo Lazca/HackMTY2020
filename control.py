48e399d7b41ed6bac432b599f25872d706aad896
Lazca <35242788+Lazca@users.noreply.github.com>
2020-08-29 15:09:51 -0500

    Update control.py

--- control.py
+++ control.py
@@ -1,3 +1,18 @@
+class Mesa:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        self.ocupantes=0;
+
+        def get_ocupantes():
+            return self.ocupantes
+
+        def set_ocupantes(n):
+            self.ocupantes=n
+
+        def vaciar():
+            self.ocupantes=0
+
 class Control:
 
     def __init__(self, m, cm, max):
@@ -12,3 +27,10 @@ class Control:
 
         for mesa in self.mesas:
             if mesa.get_ocupantes() == 0:
+                self.ocupados+=n
+                mesa.set_ocupantes(n)
+                return
+
+    def vaciar_mesa(int indice):
+        self.ocupados-=self.mesas[indice].get_ocupantes()
+        self.mesas[indice].vaciar()
