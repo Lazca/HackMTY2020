9a674f3655547670d55e5d574f95afd4176bcfc6
Lazca <35242788+Lazca@users.noreply.github.com>
2020-08-30 09:45:23 -0500

    Cambie los inputs de cupo y capacidad a readonly

--- interfaz.py
+++ interfaz.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+import tkinter
+from tkinter import *
 from control import *
 
 def getErrors():
@@ -20,6 +22,8 @@ def getErrors():
             errors.append('el radio de la mesa debe ser de al menos 0.4 m')
         if int(values[3]) < 10:
             errors.append('la capacidad normal debe ser de al menos 10 personas')
+        if int(values[7]) > int(values[6]):
+            errors.append('El ancho debe ser mayor o igual a el ancho')
 
     for error in errors:
         sg.Popup(error,title='error', keep_on_top=True, any_key_closes=True)
@@ -63,24 +67,27 @@ if __name__ == "__main__":
     sg.theme(theme)
     control=Control([],0,0)
 
-
     layout = [
-            [sg.Text("Distancia entre Mesas: ",size = (30,1)),sg.InputText()],  #0
-            [sg.Text("Radio de Mesa: ",size = (30,1)),sg.InputText()],          #1
-            [sg.Text("Espacio por Persona: ",size = (30,1)),sg.InputText()],    #2
-            [sg.Text("Capacidad Normal: ",size = (30,1)),sg.InputText()],       #3
-            [sg.Text("Porcentaje de Capacidad: ",size = (30,1)),sg.InputText()],#4
-            [sg.Text("Capacidad por Mesa: ",size = (30,1)),sg.InputText()],     #5
-            [sg.Text("Ancho del Restaurante: ",size = (30,1)),sg.InputText()],  #6
-            [sg.Text("Largo del Restaurante: ",size = (30,1)),sg.InputText(),], #7
-            [sg.Text("Gente Dentro: ",size=(30,1),text_color = "green"),sg.Input()],        #8
-            [sg.Text("Cupo Limite: ",size=(30,1),text_color = "green"),sg.Input()],         #9
-            [sg.Text("Sentar Grupo: ",size=(30,1),text_color = "green"),sg.InputText()],    #10
-            [sg.Text("Desalojar Mesa: ",size=(30,1),text_color = "green"),sg.InputText()],  #11
+            [sg.Text("Distancia entre Mesas: ",size = (30,1)),sg.InputText()],                          #0
+            [sg.Text("Radio de Mesa: ",size = (30,1)),sg.InputText()],                                  #1
+            [sg.Text("Espacio por Persona: ",size = (30,1)),sg.InputText()],                            #2
+            [sg.Text("Capacidad Normal: ",size = (30,1)),sg.InputText()],                               #3
+            [sg.Text("Porcentaje de Capacidad: ",size = (30,1)),sg.InputText()],                        #4
+            [sg.Text("Capacidad por Mesa: ",size = (30,1)),sg.InputText()],                             #5
+            [sg.Text("Ancho del Restaurante: ",size = (30,1)),sg.InputText()],                          #6
+            [sg.Text("Largo del Restaurante: ",size = (30,1)),sg.InputText(),],                         #7
+            [sg.Text("Gente Dentro: ",size=(30,1),text_color = "green"),sg.InputText(disabled=true)],                    #8
+            [sg.Text("Cupo Limite: ",size=(30,1),text_color = "green"),sg.InputText(disabled=True)],                     #9
+            [sg.Text("Sentar Grupo de tamaño: ",size=(30,1),text_color = "green"),sg.InputText()],      #10
+            [sg.Text("Desalojar Mesa No: ",size=(30,1),text_color = "green"),sg.InputText()],           #11
             [sg.Button("Enviar", size = (30,1)), sg.Button("Dibujar", size = (30,1))],
+            [sg.Button("Sentar", size = (30,1)), sg.Button("Desalojar mesa", size = (30,1))],
             [sg.Graph(canvas_size=(500, 500), \
             graph_bottom_left=(0,0), graph_top_right=(500, 500), background_color='white', key='graph',drag_submits = True)]
            ]
+
+    for label in layout:
+        print(label)
 
     window = sg.Window("Asignador",layout,finalize = True,icon = "restaurant.ico")
     window.Maximize()
@@ -89,7 +96,7 @@ if __name__ == "__main__":
         event,values = window.read()
         print(values)
         lsTable = update(values,control)
-        
+
         if event == "Dibujar" and lsTable:
             control.setMaxCapacity(100)
 
@@ -108,7 +115,7 @@ if __name__ == "__main__":
 
             h = int(values[7])/int(values[6])*475
             offset = (500 - h)/2
-            
+
             for index, table in enumerate(lsTable):
                 x,y = table.position
                 x = remap(x,0,int(values[6]),25,475)
@@ -118,10 +125,10 @@ if __name__ == "__main__":
                     fill_color = 'green'
                 else:
                     fill_color = 'red'
-                        
+
                 graph.DrawCircle((x,y), 10, fill_color,line_color='black')
                 graph.DrawText(str(index), (x,y), color='white')
-                
+
         if event == sg.WIN_CLOSED:
             break
 
